3dbc8261bc9009c666c23baf165165528fdef042
Fredrik Johansen <fredrik1206@gmail.com>
2025-12-22 11:45:11 +0100

    feat(app): adds fetching of version number to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const fetchVersion = async (): Promise<{ version: string }> => {
+  const response = await fetch("/version.json");
+  return response.json();
+};
+
+export const Version = () => {
+  const { data } = useQuery({ queryKey: ["version"], queryFn: fetchVersion });
+
+  return <p>Version: {data?.version ?? ""}</p>;
+};
+
 export const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <h1 className="text-5xl">Hello World!</h1>
-      <p>Lorem Ipsum...</p>
+      <QueryClientProvider client={queryClient}>
+        <h1 className="text-5xl">Hello World!</h1>
+        <p>Lorem Ipsum...</p>
+        <Version />
+      </QueryClientProvider>
     </>
   );
 };
